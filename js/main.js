9c4739638be14ebd0ee449d742ce5b4c3fa3f55c
Hajime Fukuda <hajime.fukuda@me.com>
2017-10-29 14:58:03 +0900

    unified rikei and bunkei

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-$(() => {
-  let rikei = [];
-  let bunkei = [];
-
-  let bunkeiAffiliation = ["文化", "社会", "学際"];
-  for (let d of data) {
-    if (bunkeiAffiliation.some(x => d.affiliation.includes(x))) {
-      bunkei.push(d);
-    } else {
-      rikei.push(d);
-    }
-  }
-
-  let template = $('#template').html();
+$(function() {
+  var template = $('#template').html();
   Mustache.parse(template);
-  let renderedRikei = Mustache.render(template, {talks: rikei, header: "理工系講演"});
-  let renderedBunkei = Mustache.render(template, {talks: bunkei, header: "人文系講演"});
-  $('.article-headline').html(renderedRikei + renderedBunkei);
+  var rendered = Mustache.render(template, {talks: data});
+  $('.article-headline').html(rendered);
 });
